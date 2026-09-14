f5fd1f183b4a4adf1aba127a3286a0d8ec1f4d7f
Pinpin04 <162092271+Pinpin04@users.noreply.github.com>
2026-09-14 12:31:07 +0900

    입력받은 트리의 정보를 출력하는 프로그램 작성

--- test.c
+++ test.c
@@ -1,30 +1,32 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <string.h>
 #include <ctype.h>
 
 #define MAX_LEN 500
 
-// --- 스택 구현 ---
 typedef struct {
     int data[MAX_LEN];
     int top;
 } Stack;
 
+/* Initialize stack */
 void initStack(Stack* s) {
     s->top = -1;
 }
 
+/* Check empty */
 int isEmpty(Stack* s) {
     return s->top == -1;
 }
 
+/* Push data */
 void push(Stack* s, int val) {
     if (s->top < MAX_LEN - 1) {
         s->data[++(s->top)] = val;
     }
 }
 
+/* Pop data */
 int pop(Stack* s) {
     if (!isEmpty(s)) {
         return s->data[(s->top)--];
@@ -32,6 +34,7 @@ int pop(Stack* s) {
     return 0;
 }
 
+/* Check top */
 int peek(Stack* s) {
     if (!isEmpty(s)) {
         return s->data[s->top];
@@ -39,42 +42,76 @@ int peek(Stack* s) {
     return 0;
 }
 
-// --- 문자열 유효성 검사 ---
+/* Check tree notation */
 int isValidTreeString(const char* s) {
     int len = strlen(s);
-    if (len == 0) return 0;
+
+    if (len == 0)
+        return 0;
 
     Stack st;
     initStack(&st);
 
-    for (int i = 0; i < len; ++i) {
+    int expectingNode = 1;
+    int nodeCount = 0;
+
+    for (int i = 0; i < len; i++) {
         char ch = s[i];
+
         if (isupper((unsigned char)ch)) {
-            if (i + 1 < len) {
-                char next = s[i + 1];
-                if (next != '(' && next != ',' && next != ')') return 0;
-            }
+
+            if (!expectingNode)
+                return 0;
+
+            nodeCount++;
+            expectingNode = 0;
+
         } else if (ch == '(') {
-            if (i == 0 || !isupper((unsigned char)s[i - 1])) return 0;
+
+            if (expectingNode)
+                return 0;
+
             push(&st, '(');
-        } else if (ch == ')') {
-            if (isEmpty(&st)) return 0;
-            pop(&st);
+            expectingNode = 1;
+
         } else if (ch == ',') {
-            if (i == 0 || i == len - 1) return 0;
+
+            if (isEmpty(&st) || expectingNode)
+                return 0;
+
+            expectingNode = 1;
+
+        } else if (ch == ')') {
+
+            if (isEmpty(&st) || expectingNode)
+                return 0;
+
+            pop(&st);
+            expectingNode = 0;
+
         } else {
-            return 0; // 허용되지 않은 문자
+            return 0;
         }
     }
-    return isEmpty(&st);
+
+    if (!isEmpty(&st) || expectingNode)
+        return 0;
+
+    if (nodeCount == 0)
+        return 0;
+
+    return 1;
 }
 
 int main(void) {
     char input[MAX_LEN];
-    printf("트리 괄호 표기법 입력: ");
-    if (scanf("%s", input) != 1) return 0;
 
-    // 1. 유효성 검사
+    printf("트리 괄호 표기법 입력: ");
+
+    if (scanf("%499s", input) != 1) {
+        return 0;
+    }
+
     if (!isValidTreeString(input)) {
         printf("오류: 올바르지 않은 트리의 괄호 표기법입니다.\n");
         return 0;
@@ -86,9 +123,9 @@ int main(void) {
     int maxHeight = 0;
     int maxDegree = 0;
 
-    // 스택 정의
-    Stack nodeStack;       // 부모 노드 추적용 (문자 저장)
-    Stack childCountStack; // 노드별 자식 수 카운트용 (정수 저장)
+    Stack nodeStack;
+    Stack childCountStack;
+
     initStack(&nodeStack);
     initStack(&childCountStack);
 
@@ -101,69 +138,83 @@ int main(void) {
 
     printf("\n[트리 계층 구조 출력]\n");
 
-    for (int i = 0; i < len; ++i) {
+    for (int i = 0; i < len; i++) {
         char ch = input[i];
 
+        /* Process node */
         if (isupper((unsigned char)ch)) {
+
             totalNodes++;
 
-            // 높이 갱신
+            /* Calculate height */
             if (currentDepth + 1 > maxHeight) {
                 maxHeight = currentDepth + 1;
             }
 
-            // 노드 C의 부모 확인
-            if (ch == 'C') {
-                if (!isEmpty(&nodeStack)) {
-                    parentOfC = (char)peek(&nodeStack);
-                }
+            /* Find C's parent */
+            if (ch == 'C' && !isEmpty(&nodeStack)) {
+                parentOfC = (char)peek(&nodeStack);
             }
 
-            // 노드 C의 자식 확인
-            if (!isEmpty(&nodeStack) && peek(&nodeStack) == 'C') {
+            /* Find C's children */
+            if (!isEmpty(&nodeStack) &&
+                peek(&nodeStack) == 'C') {
+
                 childrenOfC[childrenOfCCount++] = ch;
             }
 
-            // 현재 노드가 자식을 가지는지 여부 (다음 문자가 '(' 인지)
-            int hasChild = (i + 1 < len && input[i + 1] == '(');
+            /* Check leaf */
+            int hasChild =
+                (i + 1 < len && input[i + 1] == '(');
 
             if (hasChild) {
                 nonLeafNodes++;
             } else {
                 leafNodes++;
-                // 부모의 자식 수 증가
+
+                /* Count child */
                 if (!isEmpty(&childCountStack)) {
                     int cnt = pop(&childCountStack);
                     push(&childCountStack, cnt + 1);
                 }
             }
 
-            // 계층적 출력
+            /* Print tree */
             if (currentDepth == 0) {
                 printf("%c\n", ch);
             } else {
-                for (int d = 0; d < currentDepth - 1; ++d) {
+                for (int d = 0; d < currentDepth - 1; d++) {
                     printf("    ");
                 }
+
                 printf("+---%c\n", ch);
             }
 
+        /* Open parenthesis */
         } else if (ch == '(') {
+
             char parent = input[i - 1];
+
             push(&nodeStack, parent);
             push(&childCountStack, 0);
+
             currentDepth++;
 
+        /* Close parenthesis */
         } else if (ch == ')') {
+
             currentDepth--;
 
             int currentDegree = pop(&childCountStack);
+
             pop(&nodeStack);
 
+            /* Update degree */
             if (currentDegree > maxDegree) {
                 maxDegree = currentDegree;
             }
 
+            /* Count subtree */
             if (!isEmpty(&childCountStack)) {
                 int cnt = pop(&childCountStack);
                 push(&childCountStack, cnt + 1);
@@ -171,26 +222,31 @@ int main(void) {
         }
     }
 
-    // 결과 출력
     printf("\n[트리 정보 출력]\n");
+
     printf("* 전체 노드의 수: %d\n", totalNodes);
     printf("* 단말 노드의 수: %d\n", leafNodes);
     printf("* 비단말 노드의 수: %d\n", nonLeafNodes);
     printf("* 트리의 높이: %d\n", maxHeight);
     printf("* 트리의 차수: %d\n", maxDegree);
 
+    /* Print C's parent */
     if (parentOfC != '\0') {
         printf("* 노드 C의 부모 노드: %c\n", parentOfC);
     } else {
         printf("* 노드 C의 부모 노드: 없음\n");
     }
 
+    /* Print C's children */
     printf("* 노드 C의 자식 노드: ");
+
     if (childrenOfCCount == 0) {
         printf("없음\n");
     } else {
-        for (int i = 0; i < childrenOfCCount; ++i) {
-            printf("%c%s", childrenOfC[i], (i == childrenOfCCount - 1) ? "" : ", ");
+        for (int i = 0; i < childrenOfCCount; i++) {
+            printf("%c%s",
+                   childrenOfC[i],
+                   (i == childrenOfCCount - 1) ? "" : ", ");
         }
         printf("\n");
     }
